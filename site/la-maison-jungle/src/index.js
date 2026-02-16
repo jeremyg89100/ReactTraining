5bbef3f42d38df72a59e218bfcc7f168b9c07705
jeremyg <jeremygirard89100@gmail.com>
2026-02-16 15:29:22 +0100

    add footer, plantItem, careScale

--- site/la-maison-jungle/src/index.js
+++ site/la-maison-jungle/src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Banner from "./components/Banner";
-import Cart from "./components/Cart";
-import ShoppingList from "./components/ShoppingList";
+import "./styles/index.css";
+import App from "./components/App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Banner />
-    <Cart />
-    <ShoppingList />
+    <App />
   </React.StrictMode>,
   document.getElementById("root"),
 );
