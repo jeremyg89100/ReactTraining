8b1bad63a4348b21c5b4d38557875cc6cb95ec79
jeremyg <jeremygirard89100@gmail.com>
2026-02-17 10:35:15 +0100

    add categories, cart, shoppingList

--- site/la-maison-jungle/src/components/App.js
+++ site/la-maison-jungle/src/components/App.js
@@ -1,18 +1,21 @@
+import { useState } from "react";
 import Banner from "./Banner";
 import logo from "../assets/logo.png";
-// import Cart from "./Cart";
+import Cart from "./Cart";
 import Footer from "./Footer";
 import ShoppingList from "./ShoppingList";
 
 function App() {
+  const [cart, updateCart] = useState([]);
+
   return (
     <div>
       <Banner>
         <img src={logo} alt="La maison jungle" className="lmj-logo" />
         <h1 className="lmj-title">La maison jungle</h1>
       </Banner>
-      {/* <Cart /> */}
-      <ShoppingList />
+      <Cart cart={cart} updateCart={updateCart} />
+      <ShoppingList cart={cart} updateCart={updateCart} />
       <Footer />
     </div>
   );
